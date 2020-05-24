0845aeca5bbabe5c2e07e52391fd350f3fb511be
quinten Kasteel <quintenkasteel@gmail.com>
2020-05-24 19:13:46 +0200

    commit

--- src/Library/components/Sticky.js
+++ src/Library/components/Sticky.js
@@ -1,33 +1,33 @@
-import React from "react"
-import styled from "styled-components"
+import React from 'react';
+import styled from 'styled-components';
+import propTypes from 'prop-types';
 
 class Sticky extends React.Component {
   state = {
     scrollPostion: 0,
     stuck: false,
-  }
+  };
 
   listenToScrollEvent = () => {
-    document.addEventListener("scroll", () => {
+    document.addEventListener('scroll', () => {
       requestAnimationFrame(() => {
-        this.calculateScrollDistance()
-      })
-    })
-  }
+        this.calculateScrollDistance();
+      });
+    });
+  };
 
   calculateScrollDistance = () => {
     const scrollTop =
       window.pageYOffset ||
-      (document.documentElement || document.body.parentNode || document.body)
-        .scrollTop // how much the user has scrolled by
+      (document.documentElement || document.body.parentNode || document.body).scrollTop; // how much the user has scrolled by
 
     this.setState({
       scrollPostion: scrollTop,
-    })
-  }
+    });
+  };
 
   componentDidMount() {
-    this.listenToScrollEvent()
+    this.listenToScrollEvent();
   }
 
   render() {
@@ -45,46 +45,55 @@ class Sticky extends React.Component {
       sticky,
       fixed,
       borderRadius,
-      margin
-    } = this.props
+      margin,
+    } = this.props;
 
-    const { stuck, scrollPostion } = this.state
+    const { scrollPostion } = this.state;
 
-    const fixedFrom = scrollPostion >= from ? "fixed" : null
+    const fixedFrom = scrollPostion >= from ? 'fixed' : null;
 
     const StickyContainer = styled.div`
-      position: ${sticky
-        ? "sticky"
-        : fixed && from
-        ? fixedFrom
-        : fixed
-        ? "fixed"
-        : "sticky"};
+      position: ${sticky ? 'sticky' : fixed && from ? fixedFrom : fixed ? 'fixed' : 'sticky'};
       display: flex;
       flex-flow: column wrap;
       padding: 0.625rem;
       margin: ${margin ? margin : `0.625rem`};
       z-index: 1000;
-      border-radius: ${borderRadius ? borderRadius : ""};
-      background: ${background ? background : "white"};
-      top: ${top && offset ? offset : top ? "0" : "auto"};
-      left: ${left && offset ? offset : left ? "0" : "auto"};
-      right: ${right && offset ? offset : right ? "0" : "auto"};
-      bottom: ${bottom && offset ? offset : bottom ? "0" : "auto"};
+      border-radius: ${borderRadius ? borderRadius : ''};
+      background: ${background ? background : 'white'};
+      top: ${top && offset ? offset : ''};
+      top: ${top && !offset ? top : '0'};
+      top: ${!top && !offset ? 'auto' : ''};
+      left: ${left && offset ? offset : left ? '0' : 'auto'};
+      right: ${right && offset ? offset : right ? '0' : 'auto'};
+      bottom: ${bottom && offset ? offset : bottom ? '0' : 'auto'};
       flex: ${fillVertical && fillHorizontal
-        ? "1 1 auto"
+        ? '1 1 auto'
         : fillVertical
-        ? "1 0 auto"
+        ? '1 0 auto'
         : fillHorizontal
-        ? "0 1 auto"
-        : "0 0 auto"};
-    `
-    return (
-      <StickyContainer className="sticky-scontainer">
-        {children}
-      </StickyContainer>
-    )
+        ? '0 1 auto'
+        : '0 0 auto'};
+    `;
+    return <StickyContainer className="sticky-scontainer">{children}</StickyContainer>;
   }
 }
 
-export default Sticky
+Sticky.propTypes = {
+  children: propTypes.node,
+  offset: propTypes.number,
+  from: propTypes.number,
+  top: propTypes.string,
+  left: propTypes.string,
+  bottom: propTypes.string,
+  right: propTypes.string,
+  fillHorizontal: propTypes.bool,
+  fillVertical: propTypes.bool,
+  background: propTypes.string,
+  sticky: propTypes.bool,
+  fixed: propTypes.bool,
+  borderRadius: propTypes.string,
+  margin: propTypes.string,
+};
+
+export default Sticky;
--- src/Library/components/Tabs.js
+++ src/Library/components/Tabs.js
@@ -1,77 +1,85 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
+import React from 'react';
+import propTypes from 'prop-types';
+import styled from 'styled-components';
 
 const TabsContainer = styled.div`
-	width: 100%;
+  width: 100%;
 `;
+
 const TabsHeader = styled.div`
-	display: inline-flex;
-	list-style: none;
-	width: 50%;
-	justify-content: space-around;
+  display: inline-flex;
+  list-style: none;
+  width: 50%;
+  justify-content: space-around;
 `;
+
 const Tab = styled.div`
-	position: relative;
-	border: 1px solid #cacaca;
-	text-align: center;
-	line-height: 25px;
-	padding: 0.5rem;
-	width: 100%;
-	outline: none;
-	margin-bottom: 0.625rem;
+  position: relative;
+  border: 1px solid #cacaca;
+  text-align: center;
+  line-height: 25px;
+  padding: 0.5rem;
+  width: 100%;
+  outline: none;
+  margin-bottom: 0.625rem;
 
-	&:first-child {
-		border-top-left-radius: 5px;
-		border-bottom-left-radius: 5px;
-	}
+  &:first-child {
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+  }
 
-	&:last-child {
-		border-top-right-radius: 5px;
-		border-bottom-right-radius: 5px;
-	}
+  &:last-child {
+    border-top-right-radius: 5px;
+    border-bottom-right-radius: 5px;
+  }
 
-	&.selected {
-		border-bottom: 2px solid blue;
-	}
+  &.selected {
+    border-bottom: 2px solid blue;
+  }
 `;
 
 class Tabs extends React.Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			selected: this.props.selected || 0
-		};
-		this.handleChange = this.handleChange.bind(this);
-	}
+    this.state = {
+      selected: this.props.preSelected || 0,
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-	handleChange(index) {
-		this.setState({ selected: index });
-	}
+  handleChange(index) {
+    this.setState({ selected: index });
+  }
 
-	render() {
-		return this.props.children ? (
-			<TabsContainer className="tabs">
-				<TabsHeader className="tabs-header">
-					{this.props.children.map((content, index) => {
-						let selected = index == this.state.selected ? "selected" : "";
-						return (
-							<Tab
-								className={`tab ${selected}`}
-								key={index}
-								onClick={this.handleChange.bind(this, index)}>
-								{content.props.title}
-							</Tab>
-						);
-					})}
-				</TabsHeader>
-				<div className="tab-content">
-					{this.props.children[this.state.selected]}
-				</div>
-			</TabsContainer>
-		) : null;
-	}
+  render() {
+    const { children } = this.props;
+    const { selected } = this.state;
+
+    return children ? (
+      <TabsContainer className="tabs">
+        <TabsHeader className="tabs-header">
+          {children.map((content, index) => {
+            const selectedTab = index === selected ? 'selected' : '';
+            return (
+              <Tab
+                className={`tab ${selectedTab}`}
+                key={index}
+                onClick={this.handleChange.bind(this, index)}>
+                {content.props.title}
+              </Tab>
+            );
+          })}
+        </TabsHeader>
+        <div className="tab-content">{children[this.state.selected]}</div>
+      </TabsContainer>
+    ) : null;
+  }
 }
+
+Tabs.propTypes = {
+  children: propTypes.node,
+  preSelected: propTypes.number,
+};
 
 export default Tabs;
