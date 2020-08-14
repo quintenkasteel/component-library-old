fb0453c59e9416767ec30eacb39f6a35e311553e
Quinten Kasteel <kasteel@floydhamilton.nl>
2020-08-14 22:30:32 +0200

    Functional accordion

--- src/Library/components/Accordion.js
+++ src/Library/components/Accordion.js
@@ -1,61 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
 const AccordionContainer = styled.div`
-	width: 100%;
-	display: flex;
-	flex-direction: column;
-	flex-basis: 100%;
-	width: 100%;
-	position: relative;
-	border: 1px solid #cacaca;
-	border-radius: 5px;
-	line-height: 25px;
-	padding: 0.5rem;
-	width: 100%;
-	outline: none;
-	margin-bottom: 0.625rem;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  flex-basis: 100%;
+  width: 100%;
+  position: relative;
+  border: 1px solid #cacaca;
+  border-radius: 5px;
+  line-height: 25px;
+  padding: 0.5rem;
+  width: 100%;
+  outline: none;
+  margin-bottom: 0.625rem;
 `;
 const AccordionBody = styled.div`
-	display: ${props => (props.open ? "block" : "none")};
-	padding: 0.625rem 0;
+  display: ${(props) => (props.show ? "block" : "none")};
+  padding: 0.625rem 0;
 `;
 
 const AccordionTitle = styled.h3``;
 
-class Accordion extends React.Component {
-	constructor(props) {
-		super(props);
+const Accordion = ({label, labeltype, icon, disabled, children}) => {
+  const [open, setOpen] = useState(false);
+  const isOpen = open ? "open" : "";
 
-		this.state = {
-			open: false
-		};
-		this.handleChange = this.handleChange.bind(this);
-	}
-	handleChange() {
-		this.setState({ open: !this.state.open });
-	}
-
-	render() {
-		const { label, labeltype, icon, disabled, children } = this.props;
-		const open = this.state.open ? "open" : "";
-
-		return (
-			<React.Fragment>
-				<AccordionContainer className={`accordion-container ${open}`}>
-					<AccordionTitle
-						className="accordion-title"
-						onClick={() => this.handleChange()}>
-						{label}
-					</AccordionTitle>
-					<AccordionBody open={this.state.open} className="accordion-body">
-						{children}
-					</AccordionBody>
-				</AccordionContainer>
-			</React.Fragment>
-		);
-	}
-}
+  return (
+    <React.Fragment>
+      <AccordionContainer className={`accordion-container ${isOpen}`}>
+        <AccordionTitle
+          className="accordion-title"
+          onClick={() => setOpen(!open)}
+        >
+          {label}
+        </AccordionTitle>
+        <AccordionBody show={open} className="accordion-body">
+          {children}
+        </AccordionBody>
+      </AccordionContainer>
+    </React.Fragment>
+  );
+};
 
 export default Accordion;
